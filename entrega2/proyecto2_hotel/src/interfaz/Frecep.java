8126ff1a4b894037092756b5f5bbe18cd3743c32
RuizL25 <99106138+RuizL25@users.noreply.github.com>
2023-05-02 10:34:03 -0500

    color del panel recepcionista

--- entrega2/proyecto2_hotel/src/interfaz/Frecep.java
+++ entrega2/proyecto2_hotel/src/interfaz/Frecep.java
@@ -6,13 +6,20 @@ import java.awt.*;
 
 public class Frecep extends JFrame {
 
+    public JLabel titulo;
+    public JPanel panel;
+
     public Frecep() {
 
         this.setTitle("Motel los pps");
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setSize(1220, 800);
+        this.panel = new JPanel(getLayout());
+        this.panel.setBackground(new Color(28, 35, 46));
 
         this.setLayout(new BorderLayout());
+
+        this.add(this.panel, BorderLayout.CENTER);
         this.setLocationRelativeTo(null);
         this.setVisible(true);
     }
